49db6358ab9852e9e2d39500c3b47e5b64e80fa7
Alexis JC <alexisjc.dev@gmail.com>
2022-09-29 16:33:41 +0200

    chore: update designSystemId

--- ww-config.js
+++ ww-config.js
@@ -1,8 +1,5 @@
 export default {
     editor: {
-        // PROD
-        // designSystemId: '40cf1c33-2c6e-4d37-b54d-7c877872e235',
-        // STAGING
-        designSystemId: '8b81bd01-e1cd-40a1-866e-782c099a9a5d',
+        designSystemId: 'bf0afd2b-e049-43c4-a15b-a3404b877a1e',
     },
 };
