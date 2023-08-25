fda5f6770a2b9fc7984f9fe85d2cab1a81f43a82
Alexis JC <alexisjc.dev@gmail.com>
2023-08-25 16:56:42 +0200

    feat: move mapbox inside local variable

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -1,9 +1,5 @@
 import mapboxgl from 'mapbox-gl';
 
-import { markRaw } from 'vue';
-
 export default {
-    async onLoad() {
-        wwLib.wwVariable.updateValue(`${this.id}-mapboxgl`, markRaw(mapboxgl));
-    },
+    mapboxgl,
 };
