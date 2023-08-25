499e5244900c77ec319a436a242fdb55a7cdfb9c
Alexis JC <alexisjc.dev@gmail.com>
2023-08-25 16:33:40 +0200

    feat: add label only and markRaw

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -1,6 +1,9 @@
 import mapboxgl from 'mapbox-gl';
+
+import { markRaw } from 'vue';
+
 export default {
     async onLoad() {
-        wwLib.wwVariable.updateValue(`${this.id}-mapboxgl`, mapboxgl);
+        wwLib.wwVariable.updateValue(`${this.id}-mapboxgl`, markRaw(mapboxgl));
     },
-}
+};
--- ww-config.js
+++ ww-config.js
@@ -2,7 +2,5 @@ export default {
     editor: {
         designSystemId: 'bf0afd2b-e049-43c4-a15b-a3404b877a1e',
     },
-    variables: [
-        { name: 'mapboxgl', value: 'mapboxgl', type: 'object', defaultValue: null },
-    ],
+    variables: [{ name: 'mapboxgl', value: 'mapboxgl', type: 'object', defaultValue: null, labelOnly: '{Mapboxgl}' }],
 };
