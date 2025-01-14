994b9c0ff2e09feddbf60880955802a9242e07fb
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:23:07 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -2,4 +2,15 @@ export default {
     editor: {
         designSystemId: 'bf0afd2b-e049-43c4-a15b-a3404b877a1e',
     },
+    actions: {
+        /* wwEditor:start */
+        getMapboxInstance: {
+            copilot: {
+                description: 'Get the Mapbox GL JS instance to access its full API',
+                returns: 'object',
+                schema: {}
+            }
+        },
+        /* wwEditor:end */
+    }
 };
